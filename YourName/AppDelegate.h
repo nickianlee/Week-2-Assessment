1406e56e14e786fc6b47daec3dca41d8cc2cc693
nicholaslee <nicholaslee@nicholaslees-MacBook-Pro.local>
2017-03-27 10:17:34 +0800

    Initial Commit

---
+++ YourName/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YourName
+//
+//  Created by nicholaslee on 27/03/2017.
+//  Copyright © 2017 nicholaslee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YourName/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YourName
+//
+//  Created by nicholaslee on 27/03/2017.
+//  Copyright © 2017 nicholaslee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
